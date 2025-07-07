d7c706d1c8179d06a91594bf8692cfb5c6f63870
Denaisha Kraft <denaisha.k@gmail.com>
2025-07-07 23:36:55 +0000

    dataManager update

---
+++ data/atlas.py
@@ -0,0 +1,41 @@
+import torch
+import h5py
+import numpy as np
+from collections import defaultdict
+
+def get_atlas_dataset(cfg):
+    with h5py.File(cfg.data.path, 'r') as f:
+        showers = torch.tensor(f["showers"][:]).float()
+        energies = torch.tensor(f["incident_energies"][:]).float().squeeze()
+
+    energies_np = energies.numpy()
+    unique_energies = np.unique(energies_np)
+    is_discrete = len(unique_energies) < 20 # only ~15 unique so just a val bigger
+
+    if is_discrete:
+        bin_ids = np.digitize(energies_np, unique_energies, right=False)
+    else: # using smeared logic
+        energy_bin_centers = [2**i for i in range(8, 23)]
+        energy_bin_edges = [2**(np.log2(c) - 0.5) for c in energy_bin_centers]
+        energy_bin_edges.append(2**(np.log2(energy_bin_centers[-1]) + 0.5))
+        bin_ids = np.digitize(energies_np, energy_bin_edges, right=False)
+
+    bin_to_indices = defaultdict(list)
+    for i, b in enumerate(bin_ids):
+        bin_to_indices[b].append(i)
+
+    train_idx, val_idx, test_idx = [], [], []
+    for indices in bin_to_indices.values():
+        np.random.shuffle(indices)
+        n = len(indices)
+        n_train = int(cfg.data.frac_train_dataset * n)
+        n_val = int(cfg.data.frac_val_dataset * n)
+        n_test = n - n_train - n_val
+
+        train_idx.extend(indices[:n_train])
+        val_idx.extend(indices[n_train:n_train + n_val])
+        test_idx.extend(indices[n_train + n_val:])
+
+    ordered_indices = np.concatenate([train_idx, val_idx, test_idx])
+    return { "showers": showers[ordered_indices],
+             "incident_energies": energies[ordered_indices].unsqueeze(1)}
--- data/dataManager.py
+++ data/dataManager.py
@@ -2,8 +2,12 @@ import torch
 import h5py
 import numpy as np
 from torch.utils.data import DataLoader, Dataset
-
 from CaloQuVAE import logging
+
+# for specific datasets
+from data.atlas import get_atlas_dataset
+from data.calo import get_calo_dataset
+
 logger = logging.getLogger(__name__)
 
 class CaloDataset(Dataset):
@@ -18,10 +22,10 @@ class CaloDataset(Dataset):
 
 class DataManager():
     def __init__(self, cfg=None):
-        self._config=cfg
-        self.load_dataset()
-        self.create_dataloaders()
-
+        self._config = cfg
+        self.load_dataset() 
+        self.select_dataset()          # specific datasets
+        self.create_dataloaders()      # slice into train/val/test
 
     def load_dataset(self):
         with h5py.File(self._config.data.path, 'r') as file:
@@ -34,31 +38,52 @@ class DataManager():
 
         logger.info(f'{self.f.keys()}')
 
-    def  create_dataloaders(self):
-        tr = int(np.floor(self.f["showers"].shape[0] * self._config.data.frac_train_dataset))
-        va = int(np.floor(self.f["showers"].shape[0] * self._config.data.frac_val_dataset))
+    def select_dataset(self):
+        dataset_name = self._config.data.dataset_name.lower()
 
+        if "atlas" in dataset_name:
+            logger.info(f"Loading ATLAS dataset: {self._config.data.dataset_name}")
+            self.f = get_atlas_dataset(self._config)
+
+        elif "calo" in dataset_name:
+            logger.info(f"Loading CaloChallenge dataset: {self._config.data.dataset_name}")
+            self.f = get_calo_dataset(self._config)
+
+        else:
+            raise ValueError(f"Loading other dataset: '{dataset_name}")
+
+    def create_dataloaders(self):
+        total = self.f["showers"].shape[0]
+        frac_train = self._config.data.frac_train_dataset
+        frac_val = self._config.data.frac_val_dataset
+
+        tr = int(np.floor(total * frac_train))
+        va = int(np.floor(total * frac_val))
+
+        showers = self.f["showers"]
+        energies = self.f["incident_energies"]
+        
         self.train_loader = DataLoader(
-            CaloDataset((self.f["showers"][:tr,:], self.f["incident_energies"][:tr,:])),
+            CaloDataset((showers[:tr, :], energies[:tr, :])),
             batch_size=self._config.data.batch_size_tr,
             shuffle=True,
             num_workers=self._config.data.num_workers
         )
 
         self.val_loader = DataLoader(
-            CaloDataset((self.f["showers"][tr:tr+va,:], self.f["incident_energies"][tr:tr+va,:])),
+            CaloDataset((showers[tr:tr + va, :], energies[tr:tr + va, :])),
             batch_size=self._config.data.batch_size_val,
             shuffle=False,
             num_workers=self._config.data.num_workers
         )
 
         self.test_loader = DataLoader(
-            CaloDataset((self.f["showers"][tr+va:,:], self.f["incident_energies"][tr+va:,:])),
+            CaloDataset((showers[tr + va:, :], energies[tr + va:, :])),
             batch_size=self._config.data.batch_size_test,
             shuffle=False,
             num_workers=self._config.data.num_workers
         )
-
-        logger.info("{0}: {2} events, {1} batches".format(self.train_loader,len(self.train_loader),len(self.train_loader.dataset)))
-        logger.info("{0}: {2} events, {1} batches".format(self.test_loader,len(self.test_loader),len(self.test_loader.dataset)))
-        logger.info("{0}: {2} events, {1} batches".format(self.val_loader,len(self.val_loader),len(self.val_loader.dataset)))
+        
+        logger.info("{0}: {2} events, {1} batches".format(self.train_loader, len(self.train_loader), len(self.train_loader.dataset)))
+        logger.info("{0}: {2} events, {1} batches".format(self.test_loader, len(self.test_loader), len(self.test_loader.dataset)))
+        logger.info("{0}: {2} events, {1} batches".format(self.val_loader, len(self.val_loader), len(self.val_loader.dataset)))
