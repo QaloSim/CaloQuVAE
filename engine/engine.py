324e75c4b9b07523bcd6cbc9d4ebf1a6c16766e1
jquetzalcoatl <j.toledo.mx@gmail.com>
2025-07-10 21:27:52 +0000

    created eval_vae and eval_ae

--- engine/engine.py
+++ engine/engine.py
@@ -163,7 +163,68 @@ class Engine():
                         self.beta, self.slope, loss_dict["loss"]))
                     wandb.log(loss_dict)
     
-    def evaluate(self, data_loader, epoch):
+    def evaluate_vae(self, data_loader, epoch):
+        log_batch_idx = max(len(data_loader)//self._config.engine.n_batches_log_val, 1)
+        self.model.eval()
+        with torch.no_grad():
+            bs = [batch[0].shape[0] for batch in data_loader]
+            ar_size = np.sum(bs)
+            ar_input_size = self._config.data.z * self._config.data.r * self._config.data.phi
+            ar_latent_size = self._config.rbm.latent_nodes_per_p
+            
+            self.incident_energy = torch.zeros((ar_size, 1), dtype=torch.float32)
+            self.showers = torch.zeros((ar_size, ar_input_size), dtype=torch.float32)
+            self.showers_reduce = torch.zeros((ar_size, ar_input_size), dtype=torch.float32)
+            self.showers_recon = torch.zeros((ar_size, ar_input_size), dtype=torch.float32)
+            self.showers_reduce_recon = torch.zeros((ar_size, ar_input_size), dtype=torch.float32)
+            self.post_samples = torch.zeros((ar_size, ar_latent_size * 4), dtype=torch.float32)
+            self.post_logits = torch.zeros((ar_size, ar_latent_size * 3), dtype=torch.float32)
+
+            self.showers_prior = torch.zeros((ar_size, ar_input_size), dtype=torch.float32)
+            self.showers_reduce_prior = torch.zeros((ar_size, ar_input_size), dtype=torch.float32)
+            self.prior_samples = torch.zeros((ar_size, ar_latent_size * 4), dtype=torch.float32)
+            self.RBM_energy_prior = torch.zeros((ar_size, 1), dtype=torch.float32)
+            self.RBM_energy_post = torch.zeros((ar_size, 1), dtype=torch.float32)
+
+            for i, (x, x0) in enumerate(data_loader):
+                x = x.to(self.device)
+                x0 = x0.to(self.device)
+                x_reduce = self._reduce(x, x0)
+                # Forward pass
+                output = self.model((x_reduce, x0))
+                # Get prior samples
+                if not self._config.engine.train_vae_separate:
+                    prior_samples = self.model.prior.block_gibbs_sampling_cond(p0 = output[2][0])
+                    _, shower_prior = self.model.decode(prior_samples, x_reduce, x0)
+                # Compute loss
+                loss_dict = self.model.loss(x_reduce, output)
+                loss_dict["loss"] = torch.stack([loss_dict[key] * self._config.model.loss_coeff[key]  for key in loss_dict.keys() if "loss" != key]).sum()
+                for key in list(loss_dict.keys()):
+                    loss_dict['val_'+key] = loss_dict[key]
+                    loss_dict.pop(key)
+                
+                idx1, idx2 = int(np.sum(bs[:i])), int(np.sum(bs[:i+1]))
+                self.incident_energy[idx1:idx2,:] = x0.cpu()
+                self.showers[idx1:idx2,:] = x.cpu()
+                self.showers_reduce[idx1:idx2,:] = x_reduce.cpu()
+                self.showers_recon[idx1:idx2,:] = self._reduceinv(output[3], x0).cpu()
+                self.showers_reduce_recon[idx1:idx2,:] = output[3].cpu()
+                self.post_samples[idx1:idx2,:] = torch.cat(output[2],dim=1).cpu()
+                self.post_logits[idx1:idx2,:] = torch.cat(output[1],dim=1).cpu()
+                self.prior_samples[idx1:idx2,:] = torch.cat(prior_samples,dim=1).cpu()
+                self.showers_prior[idx1:idx2,:] = self._reduceinv(shower_prior, x0).cpu()
+                self.showers_reduce_prior[idx1:idx2,:] = shower_prior.cpu()
+                self.RBM_energy_prior[idx1:idx2,:] = self.model.prior.energy_exp_cond(prior_samples[0], prior_samples[1], prior_samples[2], prior_samples[3]).cpu().unsqueeze(1)
+                self.RBM_energy_post[idx1:idx2,:] = self.model.prior.energy_exp_cond(output[2][0], output[2][1], output[2][2], output[2][3]).cpu().unsqueeze(1)
+                
+                if (i % log_batch_idx) == 0:
+                        logger.info('Epoch: {} [{}/{} ({:.0f}%)]\t Batch Loss: {:.4f}'.format(epoch,
+                            i, len(data_loader),100.*i/len(data_loader),
+                            loss_dict["val_loss"]))
+                        wandb.log(loss_dict)
+            self.generate_plots()
+
+    def evaluate_ae(self, data_loader, epoch):
         log_batch_idx = max(len(data_loader)//self._config.engine.n_batches_log_val, 1)
         self.model.eval()
         with torch.no_grad():
@@ -212,64 +273,60 @@ class Engine():
                 self.post_samples[idx1:idx2,:] = torch.cat(output[2],dim=1).cpu()
                 self.post_logits[idx1:idx2,:] = torch.cat(output[1],dim=1).cpu()
 
-                if not self._config.engine.train_vae_separate:
-                    self.prior_samples[idx1:idx2,:] = torch.cat(prior_samples,dim=1).cpu()
-                    self.showers_prior[idx1:idx2,:] = self._reduceinv(shower_prior, x0).cpu()
-                    self.showers_reduce_prior[idx1:idx2,:] = shower_prior.cpu()
-                    self.RBM_energy_prior[idx1:idx2,:] = self.model.prior.energy_exp_cond(prior_samples[0], prior_samples[1], prior_samples[2], prior_samples[3]).cpu().unsqueeze(1)
-                    self.RBM_energy_post[idx1:idx2,:] = self.model.prior.energy_exp_cond(output[2][0], output[2][1], output[2][2], output[2][3]).cpu().unsqueeze(1)
-                else:
-                    # Use recon as prior
-                    self.prior_samples[idx1:idx2,:] = torch.cat(output[2],dim=1).cpu()
-                    self.showers_prior[idx1:idx2,:] = self._reduceinv(output[3], x0).cpu()
-                    self.showers_reduce_prior[idx1:idx2,:] = output[3].cpu()
+    
+                # Use recon as prior
+                self.prior_samples[idx1:idx2,:] = torch.cat(output[2],dim=1).cpu()
+                self.showers_prior[idx1:idx2,:] = self._reduceinv(output[3], x0).cpu()
+                self.showers_reduce_prior[idx1:idx2,:] = output[3].cpu()
                 if (i % log_batch_idx) == 0:
                         logger.info('Epoch: {} [{}/{} ({:.0f}%)]\t Batch Loss: {:.4f}'.format(epoch,
                             i, len(data_loader),100.*i/len(data_loader),
                             loss_dict["val_loss"]))
                         wandb.log(loss_dict)
-                        
-            if wandb.run is not None and wandb.run._settings.mode != "disabled": # Only log if wandb is enabled
-                # Calorimeter layer plots
+            self.generate_plots()
+    
+    def generate_plots(self):
+        if self._config.wandb.mode != "disabled": # Only log if wandb is enabled
+            # Calorimeter layer plots
 
-                calo_input, calo_recon, calo_sampled = plot_calorimeter_shower(
-                    cfg=self._config,
-                    showers=self.showers,
-                    showers_recon=self.showers_recon,
-                    showers_sampled=self.showers_prior,
-                    incident_energy=self.incident_energy,
-                    epoch=epoch,
-                    save_dir=None
-                )
-                
-                # Log plots
-                overall_fig, fig_energy_sum, fig_incidence_ratio, fig_target_recon_ratio, fig_sparsity = vae_plots(
-                    self.incident_energy, self.showers, self.showers_recon, self.showers_prior)
-                
-                if not self._config.engine.train_vae_separate:
-                    rbm_hist = plot_rbm_histogram(self.RBM_energy_post, self.RBM_energy_prior)
-                
-                    wandb.log({
-                        "overall_plots": wandb.Image(overall_fig),
-                        "conditioned_energy_sum": wandb.Image(fig_energy_sum),
-                        "conditioned_incidence_ratio": wandb.Image(fig_incidence_ratio),
-                        "conditioned_target_recon_ratio": wandb.Image(fig_target_recon_ratio),
-                        "conditioned_sparsity": wandb.Image(fig_sparsity),
-                        "RBM histogram": wandb.Image(rbm_hist),
-                        "calo_layer_input": wandb.Image(calo_input),
-                        "calo_layer_recon": wandb.Image(calo_recon),
-                        "calo_layer_sampled": wandb.Image(calo_sampled)       
-                    })
-                else:
-                    wandb.log({
-                        "overall_plots": wandb.Image(overall_fig),
-                        "conditioned_energy_sum": wandb.Image(fig_energy_sum),
-                        "conditioned_incidence_ratio": wandb.Image(fig_incidence_ratio),
-                        "conditioned_target_recon_ratio": wandb.Image(fig_target_recon_ratio),
-                        "conditioned_sparsity": wandb.Image(fig_sparsity),
-                        "calo_layer_input": wandb.Image(calo_input),
-                        "calo_layer_recon": wandb.Image(calo_recon)
-                    })
+            calo_input, calo_recon, calo_sampled = plot_calorimeter_shower(
+                cfg=self._config,
+                showers=self.showers,
+                showers_recon=self.showers_recon,
+                showers_sampled=self.showers_prior,
+                incident_energy=self.incident_energy,
+                epoch=epoch,
+                save_dir=None
+            )
+            
+            # Log plots
+            overall_fig, fig_energy_sum, fig_incidence_ratio, fig_target_recon_ratio, fig_sparsity = vae_plots(
+                self.incident_energy, self.showers, self.showers_recon, self.showers_prior)
+            
+            if not self._config.engine.train_vae_separate:
+                rbm_hist = plot_rbm_histogram(self.RBM_energy_post, self.RBM_energy_prior)
+            
+                wandb.log({
+                    "overall_plots": wandb.Image(overall_fig),
+                    "conditioned_energy_sum": wandb.Image(fig_energy_sum),
+                    "conditioned_incidence_ratio": wandb.Image(fig_incidence_ratio),
+                    "conditioned_target_recon_ratio": wandb.Image(fig_target_recon_ratio),
+                    "conditioned_sparsity": wandb.Image(fig_sparsity),
+                    "RBM histogram": wandb.Image(rbm_hist),
+                    "calo_layer_input": wandb.Image(calo_input),
+                    "calo_layer_recon": wandb.Image(calo_recon),
+                    "calo_layer_sampled": wandb.Image(calo_sampled)       
+                })
+            else:
+                wandb.log({
+                    "overall_plots": wandb.Image(overall_fig),
+                    "conditioned_energy_sum": wandb.Image(fig_energy_sum),
+                    "conditioned_incidence_ratio": wandb.Image(fig_incidence_ratio),
+                    "conditioned_target_recon_ratio": wandb.Image(fig_target_recon_ratio),
+                    "conditioned_sparsity": wandb.Image(fig_sparsity),
+                    "calo_layer_input": wandb.Image(calo_input),
+                    "calo_layer_recon": wandb.Image(calo_recon)
+                })
 
     
     @property
