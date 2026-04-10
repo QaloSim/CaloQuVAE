097f78452470aff83ac21a354f1067296a8a51d7
Leo <leo.lj.zhu@gmail.com>
2026-04-10 18:50:49 +0000

    learning rate scheduler added

--- scripts/run_layers.py
+++ scripts/run_layers.py
@@ -2,6 +2,7 @@ from scripts.run import *
 
 @hydra.main(config_path="../config", config_name="config_layers", version_base=None)
 def main(cfg=None):
+    set_seed(cfg.seed)
     mode = cfg.wandb.mode
     if cfg.load_state:
         logger.info(f"Loading config from {cfg.config_path}")
@@ -19,6 +20,13 @@ def main(cfg=None):
         if not is_distributed() or is_master():
             wandb.init(tags = [cfg.data.dataset_name], project=cfg.wandb.project, entity=cfg.wandb.entity, config=OmegaConf.to_container(cfg, resolve=True), mode=mode)
             wandb.watch(engine.model)
+
+    engine.lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        engine.optimiser,
+        T_max=cfg.engine.lr_scheduler_T_max,
+        eta_min=cfg.engine.lr_scheduler_eta_min,
+    )
+
     print(OmegaConf.to_yaml(cfg, resolve=True))
 
     run(engine, callback)
